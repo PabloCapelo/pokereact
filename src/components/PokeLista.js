9b10bb952b3d397cbf76169125e8d73cae3add76
Pablo Capelo <pcapelos23@fpcoruna.afundacion.org>
2024-12-12 14:45:37 +0100

    Día 2 de Pokemon, añadiendo routes...

---
+++ src/components/PokeLista.js
@@ -0,0 +1,7 @@
+const PokeLista = (props) =>{
+    return <div>
+        <h1>PokeLista</h1>
+    </div>
+}
+
+export default PokeLista;
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,7 +1,40 @@
+import axios from "axios";
 import { useState } from "react";
+import { useParams } from "react-router-dom";
 
 const Pokemon = (props) => {
     const [nivel, setNivel] = useState(1);
+    const [nombre, setNombre] = useState("");
+    const [imagenFrontUrl, setImagenFrontUrl] = useState();
+    const [imagenBackUrl, setImagenBackUrl] = useState();
+    const [baseHP, setBaseHP] = useState();
+    const [baseAttack, setBaseAttack] = useState();
+    const [baseDefense, setBaseDefense] = useState();
+
+    const params = useParams();
+
+    const ID = params.id;
+    
+
+    function getStat(nombreStat, arrayStats){
+        const filteredArray = arrayStats.filter(s => s.stat.name===nombreStat);
+        if (filteredArray.lenth==0){
+            return -1;
+        }
+        return filteredArray[0].base_stat;
+    }
+
+    // La sintaxis mas modernilla es async-await
+    axios.get("https://pokeapi.co/api/v2/pokemon/"+ ID)
+    .then(response => {
+        setNombre(response.data.name);
+        setImagenFrontUrl(response.data.sprites.front_default);
+        setImagenBackUrl(response.data.sprites.back_default);
+        setBaseHP(getStat("hp",response.data.stats));
+        setBaseAttack(getStat("attack",response.data.stats));
+        setBaseDefense(getStat("defense",response.data.stats));
+    })
+
 
     const onSubirNivel = (event) =>{
         setNivel(n => n +1);
@@ -14,19 +47,21 @@ const Pokemon = (props) => {
         // TO-DO: Use real formula
         // link 
         // This one is made up
-        return 65 + (nivel * 3); 
+        return baseHP + (nivel * 3); 
     }
     
 
     const calcularAtaque = () => {
-        return 130 + (nivel * 2);
+        return baseAttack + (nivel * 2);
     }
     const calcularDefensa = () => {
-        return 60 + (nivel * 2);
+        return baseDefense + (nivel * 2);
     }
 
     return <div>
-        <h1>Absol</h1>
+        <img src={imagenFrontUrl}/>
+        <img src={imagenBackUrl}/>
+        <h1>{nombre}</h1>
         <h2>Nivel: {nivel}</h2>
         <button onClick={onSubirNivel}>Subir nivel</button>
         <button onClick={onBajarNivel}>Bajar nivel</button>
